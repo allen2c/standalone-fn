e015646a89070ab91c725871d7b2a46d915ffdce
Allen Chou <f1470891079@gmail.com>
2024-12-08 13:59:22 +0800

    Add health function and configuration files
    - Implemented a new health function in `func.py` that processes incoming requests and returns a personalized greeting.
    - Created `func.yaml` for function metadata, including name, version, and entry point.
    - Added `requirements.txt` to specify the `fdk` dependency.
    - Updated `poetry.lock` and `pyproject.toml` to include the new dependency management for the project.

---
+++ functions/health/func.py
@@ -0,0 +1,23 @@
+import io
+import json
+import logging
+
+from fdk import response
+import typing
+
+
+def handler(ctx, data: typing.Optional[io.BytesIO] = None):
+    print(ctx, type(data))
+    name = "World"
+    try:
+        body = json.loads(data.getvalue())
+        name = body.get("name")
+    except (Exception, ValueError) as ex:
+        logging.getLogger().info("error parsing json payload: " + str(ex))
+
+    logging.getLogger().info("Inside Python Hello World function")
+    return response.Response(
+        ctx,
+        response_data=json.dumps({"message": "Hello {0}".format(name)}),
+        headers={"Content-Type": "application/json"},
+    )
